dc6d9e44ccd8377edde65c728db3c7389da7a157
fatherOfChildren568 <contact.me.dothehung@gmail.com>
2025-07-22 03:09:18 +0700

    create effect for currsor

--- webKSTL/js/main.js
+++ webKSTL/js/main.js
@@ -28,4 +28,44 @@ if (confessionToggle && confessionDropdown) {
       document.removeEventListener('touchstart', closeConfessionMenu);
     }
   });
-} 
+}
+
+// Custom cursor effect
+(function() {
+  const cursor = document.createElement('div');
+  cursor.className = 'cursor-dot';
+  document.body.appendChild(cursor);
+
+  let mouseX = 0, mouseY = 0;
+  let cursorX = 0, cursorY = 0;
+
+  function animate() {
+    cursorX += (mouseX - cursorX) * 0.18;
+    cursorY += (mouseY - cursorY) * 0.18;
+    cursor.style.transform = `translate3d(${cursorX - 12}px, ${cursorY - 12}px, 0)`;
+    requestAnimationFrame(animate);
+  }
+  animate();
+
+  document.addEventListener('mousemove', e => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  // Hover effect
+  const hoverTargets = ['.confession-card', '.confession-link', '.confession-btn'];
+  hoverTargets.forEach(sel => {
+    document.body.addEventListener('mouseover', e => {
+      if (e.target.closest(sel)) cursor.classList.add('cursor-hover');
+    });
+    document.body.addEventListener('mouseout', e => {
+      if (e.target.closest(sel)) cursor.classList.remove('cursor-hover');
+    });
+  });
+
+  // Ripple effect on click
+  document.addEventListener('mousedown', e => {
+    cursor.classList.add('cursor-click');
+    setTimeout(() => cursor.classList.remove('cursor-click'), 250);
+  });
+})(); 
